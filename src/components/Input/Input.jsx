4b9ed957fb214cfeab5640aca1f44104ee73d4fd
saharoori <saharlaor@gmail.com>
2021-12-31 22:53:34 +0200

    Controlled input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 function Input({ title, type }) {
+  const [value, setValue] = useState(type === "number" ? 0 : "");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="Input">
       <label htmlFor={title.split(" ").join("")}>{title}</label>
-      <input type={type} name={title} id={title} placeholder={`${title}...`} />
+      <input
+        type={type}
+        name={title}
+        id={title}
+        placeholder={`${title}...`}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 }
