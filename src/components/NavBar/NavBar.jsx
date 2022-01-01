fc67f0b3a6ee4e165b83a4c28e259111d7bd25cd
saharoori <saharlaor@gmail.com>
2022-01-01 21:34:55 +0200

    NavBar - Use antdesign menu component and style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,33 @@
 import React from "react";
+import { Menu } from "antd";
 import { Link } from "react-router-dom";
 
 import Logo from "../../assets/img/nutrilog_logo.png";
 import "./NavBar.css";
 
+const MenuItem = Menu.Item;
+
 function NavBar() {
   return (
-    <nav className="NavBar">
-      <Link to="/">
-        <img src={Logo} alt="Nutrilog" />
-      </Link>
+    <Menu mode="horizontal" className="NavBar">
+      <MenuItem>
+        <Link to="/">
+          <img src={Logo} alt="Nutrilog" />
+        </Link>
+      </MenuItem>
 
-      <div>
-        <Link to="/">Overview</Link>
-        <Link to="/food">Food Log</Link>
-        <Link to="/profile">Profile</Link>
+      <div className="NavBar__items">
+        <MenuItem>
+          <Link to="/">Overview</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/food">Food Log</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/profile">Profile</Link>
+        </MenuItem>
       </div>
-    </nav>
+    </Menu>
   );
 }
 
