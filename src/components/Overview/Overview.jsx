5c5895efbe3a643ddf37fa6730046259640869be
saharoori <saharlaor@gmail.com>
2022-01-03 17:03:30 +0200

    Overview - Site logo centered on page

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Overview.css";
 
+import Logo from "../../assets/img/nutrilog_logo.png";
+
 function Overview() {
-  return <div className="Overview"></div>;
+  return (
+    <div className="Overview">
+      <div className="Overview__logo">
+        <img src={Logo} alt="Nutrilog" />
+      </div>
+    </div>
+  );
 }
 
 export default Overview;
