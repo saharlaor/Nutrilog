0a89b8ea9c672a77337a0fa32e8e059cc45f65f6
saharoori <saharlaor@gmail.com>
2022-01-04 20:13:02 +0200

    Graph - Display nutrient data using chart.js

---
+++ src/components/Graph/Graph.jsx
@@ -0,0 +1,39 @@
+// External
+import React, { useState } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// CSS
+import "./Graph.css";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function Graph({ data }) {
+  // data: {labels: ["your x axis tags"],
+  // datasets: [{label: "line name", data: ["your data points (Number)"], backgroundColor: ["colors of data points"], borderWidth: "line thickness"}]}
+  const [chartData, setChartData] = useState(data);
+  return (
+    <div className="Graph">
+      <Line data={chartData} options={{ responsive: true }} />
+    </div>
+  );
+}
+
+export default Graph;
